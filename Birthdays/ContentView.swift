4cbe10b6b1d9b275ea61e20dc2c197c0dab053a2
Kelly <kelvu14@gmail.com>
2025-08-08 10:49:14 -0400

    added birthday and name feature

--- Birthdays/ContentView.swift
+++ Birthdays/ContentView.swift
@@ -8,14 +8,53 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State private var friends: [Friend] = [
+        Friend(name: "Kelly", birthday: .now),
+        Friend(name: "Anna", birthday: .now)
+    ]
+    
+    @State private var newName = ""
+    @State private var newBirthday = Date.now
+    
+    
     var body: some View {
-        VStack {
+        NavigationStack {
             
+            List(friends, id: \.name) { friend in
+    HStack {
+        Text(friend.name)
+        Spacer()
+        Text(friend.birthday, format: .dateTime.month(.wide).day().year())
+                }
+            }
+            .navigationTitle("Birthdays")
+            .safeAreaInset(edge: .bottom) {
+                
+    VStack(alignment: .center, spacing:20) {
+        Text("New Birthday")
+        .font(.headline)
+        DatePicker(selection: $newBirthday) {
+        TextField("Name", text: $newName)
+        .textFieldStyle(.roundedBorder)
         }
-        .padding()
+        
+    Button("Save"){
+        let newFriend = Friend(name: newName, birthday: newBirthday)
+        friends.append(newFriend)
+        newName = ""
+        newBirthday = .now
+        }
+    .bold()
+                }
+    .padding()
+    .background(.bar)
+                
+            }
+        }
     }
 }
+    #Preview {
+        ContentView()
+    }
 
-#Preview {
-    ContentView()
-}
